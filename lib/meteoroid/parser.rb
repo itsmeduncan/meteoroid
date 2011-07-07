b1edef0cb038bac3a1dabd9e079d29e2c8a874be
Duncan Grazier <duncan@impossiblerocket.com>
2011-07-07 14:52:21 -0400

    Parse things.

--- lib/meteoroid/parser.rb
+++ lib/meteoroid/parser.rb
@@ -2,8 +2,12 @@ class Meteoroid::Parser
 
   attr_accessor :files
 
-  def initialize *args
+  def initialize(*args)
     @files = Meteoroid::File.from(args)
+  end
+
+  def parse
+    @parse ||= @files.map(&:samples).flatten
   end
 
 end
--- spec/meteoroid/parser_spec.rb
+++ spec/meteoroid/parser_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe Meteoroid::Parser do
+
   describe "files" do
     before :each do 
       @xml = File.read(File.join(File.dirname(__FILE__), %w[ .. fixtures example.jml ]))
@@ -20,6 +21,29 @@ describe Meteoroid::Parser do
 
       files.should be_an(Array)
       files.first.should be_an(Meteoroid::File)
+    end
+  end
+
+  describe "parse" do
+    it "should returns both samples from the file" do
+      xml = File.read(File.join(File.dirname(__FILE__), %w[ .. fixtures example.jml ]))
+      File.expects(:open).times(1).returns(StringIO.new(xml))
+
+      parsed = Meteoroid::Parser.new('foo.jml').parse
+
+      parsed.size.should == 2
+    end
+
+    it "should returns samples from both files" do
+      foo = File.read(File.join(File.dirname(__FILE__), %w[ .. fixtures example.jml ]))
+      bar = File.read(File.join(File.dirname(__FILE__), %w[ .. fixtures full.jml ]))
+
+      File.expects(:open).with('foo.jml').times(1).returns(StringIO.new(foo))
+      File.expects(:open).with('bar.jml').times(1).returns(StringIO.new(bar))
+
+      parsed = Meteoroid::Parser.new('foo.jml', 'bar.jml').parse
+
+      parsed.size.should == 562
     end
   end
 end
